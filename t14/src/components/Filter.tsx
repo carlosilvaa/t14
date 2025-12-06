880e18c0f20580a348c2ab9cc8911e68eccf34d9
Gabriel Silva <gabrielsilvasnts3@gmail.com>
2025-12-06 16:29:30 +0000

    Ajuste filter

--- t14/src/components/Filter.tsx
+++ t14/src/components/Filter.tsx
@@ -124,7 +124,6 @@ const s = StyleSheet.create({
     left: 0,
     right: 0,
     bottom: 0,
-    backgroundColor: "rgba(0,0,0,0.05)",
     justifyContent: "flex-start",
     alignItems: "flex-end",
     zIndex: 1000,
